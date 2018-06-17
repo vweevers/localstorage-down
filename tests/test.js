8355ee1ce0b542e20b75999937847713ccf327d2
Vincent Weevers <mail@vincentweevers.nl>
2018-06-17 17:37:35 +0200

    update abstract-leveldown to 5.0.0

--- tests/test.js
+++ tests/test.js
@@ -7,7 +7,6 @@ require('es5-shim');
 var test   = require('tape');
 var webstorage = require('../');
 var testCommon = require('./testCommon');
-var testBuffer = new Buffer('hello');
 
 require('abstract-leveldown/abstract/leveldown-test').args(webstorage, test);
 require('abstract-leveldown/abstract/open-test').setUp(test, testCommon);
@@ -21,12 +20,12 @@ require('abstract-leveldown/abstract/open-test').tearDown(test, testCommon);
 require('abstract-leveldown/abstract/del-test').all(webstorage, test, testCommon);
 require('abstract-leveldown/abstract/put-test').all(webstorage, test, testCommon);
 require('abstract-leveldown/abstract/get-test').all(webstorage, test, testCommon);
-require('abstract-leveldown/abstract/put-get-del-test').all(webstorage, test, testCommon, testBuffer);
+require('abstract-leveldown/abstract/put-get-del-test').all(webstorage, test, testCommon);
 require('abstract-leveldown/abstract/close-test').close(webstorage, test, testCommon);
 require('abstract-leveldown/abstract/iterator-test').setUp(webstorage, test, testCommon);
 require('abstract-leveldown/abstract/iterator-test').args(test);
 require('abstract-leveldown/abstract/iterator-test').sequence(test);
-require('abstract-leveldown/abstract/iterator-test').iterator(webstorage, test, testCommon, testCommon.collectEntries);
+require('abstract-leveldown/abstract/iterator-test').iterator(webstorage, test, testCommon);
 
 // Skip snapshot test, we cannot offer such guarantees.
 // require('abstract-leveldown/abstract/iterator-test').snapshot(webstorage, test, testCommon);
@@ -34,13 +33,7 @@ require('abstract-leveldown/abstract/iterator-test').iterator(webstorage, test, 
 require('abstract-leveldown/abstract/iterator-test').tearDown(test, testCommon);
 require('abstract-leveldown/abstract/iterator-range-test').all(webstorage, test, testCommon);
 require('abstract-leveldown/abstract/chained-batch-test').all(webstorage, test, testCommon);
-require('abstract-leveldown/abstract/approximate-size-test').setUp(webstorage, test, testCommon);
-require('abstract-leveldown/abstract/approximate-size-test').args(webstorage, test, testCommon);
 
-// Skip approximateSize test, we cannot implement it.
-// require('abstract-leveldown/abstract/approximate-size-test').approximateSize(test);
-
-require('abstract-leveldown/abstract/approximate-size-test').tearDown(webstorage, test, testCommon);
 require('abstract-leveldown/abstract/batch-test').all(webstorage, test, testCommon);
 
 require('./custom-tests.js').all(webstorage, test, testCommon);
